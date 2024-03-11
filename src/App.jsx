2e38147b2eff6a20e34cb20597472dfa06ca80e3
lateralkay <k.y.wissempai@gmail.com>
2024-03-11 12:49:43 +0100

    chore: build front view

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import Nav from "./components/Nav";
+import Arrow from "./assets/arrow.png"
+
 function App() {
-  
   return (
-    <div className="text-3xl font-bold underline">
-      Hello world
+    <div>
+      <Nav />
+      {/* Hero Content */}
+      <div className="flex flex-col gap-2.5 justify-center items-center">
+        <p className="font-jakarta font-extrabold text-6xl text-center leading-tight max-w-3xl">
+          Your Magic Wand for Instant Documentation
+        </p>
+        <p className="font-sans uppercase">
+          Turn ANY process into a simple step-by step Guide
+        </p>
+        <a
+          className="px-4 py-2 my-5 bg-black rounded-lg font-mulish flex gap-3 text-sm font-bold items-center text-white"
+          href=""
+        >
+          Install Wizardshot
+          <img src={Arrow} alt="logo" className="w-3.5" />
+        </a>
+        <div className="border-8 border-violet-600 rounded-lg bg-black w-1039 h-596"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '1039': '1039px',
+      },
+      height: {
+        '596': '596px',
+      },
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans],
+        mulish: ["Mulish", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
